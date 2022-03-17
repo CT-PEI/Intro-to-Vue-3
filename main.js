61f96a0fadbc16d3ffd8b8486e8c8fe3b660a475
CallistaTan <ctan@digitaled.com>
2022-03-16 22:42:28 -0400

    CT finish L10 but has bugs. Gona check their end code.

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
 const app = Vue.createApp({
     data() {
         return {
-            cart: 0,
+            cart: [],
             premium: true
         }
     },
-    methods: {}
+    methods: {
+        updateCart(id) {
+            console.log("ctdebug push id" + id)
+            this.cart.push(id)
+        },
+        removeById(id) {
+            const index = this.cart.indexOf(id)
+            console.log("ctdebug rmv id" + index)
+            if (index > -1 )
+                this.cart.splice(index, 1)
+        }
+
+    }
 })
